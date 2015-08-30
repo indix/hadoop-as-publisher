06512d32b37d3030a86e882a010c971fb52780f7
Ashwanth Kumar <ashwanthkumar@googlemail.com>
2015-08-30 13:37:25 +0530

    Handling case where there are no running jobs

--- lib/Demand.py
+++ lib/Demand.py
@@ -4,30 +4,32 @@ class Demand:
     self.data = []
     running_jobs = doc.find("h2", {"id" : "running_jobs"}).findNextSibling("table", {"class" : "datatable"})
     table_body = running_jobs.find('tbody')
-    rows = table_body.findAll('tr')
-    for row in rows:
-        cols = row.findAll('td')
-        cols = [ele.text.strip() for ele in cols]
-        cols = [ele for ele in cols if ele] # Get rid of empty values
-        if len(cols) > 1:
-          # Columns are
-          # - Jobid
-          # - Priority
-          # - User
-          # - Name
-          # - Map % Complete
-          # - Map Total
-          # - Maps Completed
-          # - Reduce % Complete
-          # - Reduce Total
-          # - Reduces Completed
-          # - Job Scheduling Information
-          # - Diagnostic Info
-          job = {
-            "total_map": int(cols[5]),
-            "total_reduce": int(cols[8])
-          }
-          self.data.append(job)
+    if table_body != None:
+      rows = table_body.findAll('tr')
+      for row in rows:
+          cols = row.findAll('td')
+          cols = [ele.text.strip() for ele in cols]
+          cols = [ele for ele in cols if ele] # Get rid of empty values
+          if len(cols) > 1:
+            # Columns are
+            # - Jobid
+            # - Priority
+            # - User
+            # - Name
+            # - Map % Complete
+            # - Map Total
+            # - Maps Completed
+            # - Reduce % Complete
+            # - Reduce Total
+            # - Reduces Completed
+            # - Job Scheduling Information
+            # - Diagnostic Info
+            job = {
+              "total_map": int(cols[5]),
+              "total_reduce": int(cols[8])
+            }
+            self.data.append(job)
+
 
   def _pick_map(self, job):
     return job['total_map']
